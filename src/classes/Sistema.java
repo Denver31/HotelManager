34e71708d0f4e8684af0e808f2de2327d025b853
aabramov <a.abramov@ctrl2go.solutions>
2025-10-28 19:41:45 -0300

    Added classes Reserva, Sistema

---
+++ src/classes/Sistema.java
@@ -0,0 +1,61 @@
+package src.classes;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+
+public class Sistema {
+    private Map<Integer, Reserva> reservas = new HashMap<>();
+    private Map<Integer, Habitacion> habitaciones = new HashMap<>();
+    private Map<Integer, Huespede> huespedes = new HashMap<>();
+    private Map<Integer, Factura> facturas = new HashMap<>();
+    private int maxIdHabitacion;
+    private int maxIdHuespede;
+    private int maxIdFactura;
+    private int maxIdReserva;
+
+    public Sistema(String habitacionesFile, String huespedeFile, String facturaFile, String idHabitacionesFile, String idHuespedeFile, String idFacturaFile) {
+        reservas = new HashMap<>();
+        habitaciones = new HashMap<>();
+        huespedes = new HashMap<>();
+        facturas = new HashMap<>();
+        maxIdHabitacion = 0;
+        maxIdHuespede = 0;
+        maxIdFactura = 0;
+        maxIdReserva = 0;
+    }
+
+    public void agregarHabitacion(Habitacion habitacion) {
+        maxIdHabitacion++;
+        habitaciones.put(maxIdHabitacion, habitacion);
+    }
+
+    public void agregarHuespede(Huespede huespede) {
+        maxIdHuespede++;
+        huespedes.put(maxIdHuespede, huespede);
+    }
+
+    private void agregarFactura(Factura factura) {
+        maxIdFactura++;
+        facturas.put(maxIdFactura, factura);
+    }
+
+    private void borrarFactura(int idFactura) {
+        facturas.remove(idFactura);
+    }
+
+    public void agregarReserva(int idHabitacion, int idHuespede, Factura factura, LocalDate desde, LocalDate hasta) {
+        maxIdReserva++;
+        agregarFactura(factura);
+        Reserva reserva = new Reserva(idHabitacion, idHuespede, maxIdFactura, desde, hasta);
+        reservas.put(maxIdReserva, reserva);
+    }
+
+    public void borrarReserva(int idReserva) {
+        Reserva res = reservas.get(idReserva);
+        borrarFactura(res.getIdFactura());
+        reservas.remove(idReserva);
+    }
+}
