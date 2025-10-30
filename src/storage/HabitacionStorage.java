53998c16a2611b5a69e72bc940508ac3fb2d79ea
Denver31 <daniil.igoshev05@gmail.com>
2025-10-30 14:11:32 -0300

    habitacion

---
+++ src/storage/HabitacionStorage.java
@@ -0,0 +1,92 @@
+package src.storage;
+
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import src.classes.Habitacion;
+
+public class HabitacionStorage extends BaseStorage {
+
+    public HabitacionStorage(String dbPath) {
+        super(dbPath);
+    }
+
+    @Override
+    protected String getCreateTableSQL() {
+        return """
+                CREATE TABLE IF NOT EXISTS habitaciones (
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    nombre TEXT NOT NULL,
+                                    descripcion TEXT,
+                                    precio REAL NOT NULL,
+                                    capacidad INTEGER NOT NULL
+                                );
+                               \s""";
+    }
+
+    public void save(Habitacion h) {
+        String sql = "INSERT INTO habitaciones (nombre, descripcion, precio, capacidad) VALUES (?, ?, ?, ?)";
+
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, h.getNombre());
+            pstmt.setString(2, h.getDescripcion());
+            pstmt.setDouble(3, h.getPrecio());
+            pstmt.setInt(4, h.getCapacidad());
+
+            pstmt.executeUpdate();
+            System.out.println("💾 Комната успешно сохранена в БД!");
+
+        } catch (SQLException e) {
+            System.err.println("Ошибка при сохранении комнаты: " + e.getMessage());
+        }
+    }
+    public Map<Integer, Habitacion> getAll() {
+        Map<Integer, Habitacion> habitacionesMap = new HashMap<>();
+        String sql = "SELECT * FROM habitaciones";
+
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                int cap = rs.getInt("capacidad");
+                Habitacion.tipoHabitacion tipo = null;
+                switch (cap) {
+                    case 2:
+                        tipo = Habitacion.tipoHabitacion.INDIVIDUAL;
+                        break;
+                    case 3:
+                        tipo = Habitacion.tipoHabitacion.MATRIMONIAL;
+                        break;
+                    case 5:
+                        tipo = Habitacion.tipoHabitacion.COMPARTIDA;
+                        break;
+                }
+                assert tipo != null;
+                Habitacion h = new Habitacion(
+                        rs.getString("nombre"),
+                        rs.getString("descripcion"),
+                        rs.getDouble("precio"),
+                        tipo
+                );
+                int id = rs.getInt("id");
+                habitacionesMap.put(id, h);
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Ошибка при получении данных: " + e.getMessage());
+        }
+
+        return habitacionesMap;
+    }
+    }
+
+
+
